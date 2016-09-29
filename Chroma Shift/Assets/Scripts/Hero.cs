08041da5bb91f281f612de1f7792838fa32805c0
matbecker <7m4tt3r7@gmail.com>
2016-09-29 02:24:11 -0400

    Minor Hero Class and InputManager implementations
    InputManager is a singleton and uses an observer pattern. It has a
    delegate for Keyboard Events and AxisEvents. Has an event for Run and
    Jump so far
    
    Hero class has a class within it that is the heros stats. The Hero is
    subscribed to the Run and Jump method that the InputManager has and
    applies force on its RigidBody2D accordingly

---
+++ Chroma Shift/Assets/Scripts/Hero.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour {
+
+	[System.Serializable]
+	public class Stats
+	{
+		public int health;
+		public int attackPower;
+		public int defence;
+		public float attackRange;
+		public float attackSpeed;
+		public Vector2 jumpForce;
+		public Vector2 movementForce;
+	}
+
+	[SerializeField] Stats stats;
+	[SerializeField] Rigidbody2D rb;
+	// Use this for initialization
+	void Start () 
+	{
+		//register jump event
+		InputManager.Instance.Jump += Jump;
+		//register run event
+		InputManager.Instance.Run += Run;
+		rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void Jump()
+	{
+		rb.AddForce(stats.jumpForce);
+	}
+	public void Run(float horizontalAxis)
+	{
+		if (horizontalAxis > 0)
+		{
+			rb.AddForce(stats.movementForce);
+		}
+		if (horizontalAxis < 0)
+		{
+			rb.AddForce(-stats.movementForce);
+		}
+	}
+}
---
+++ Chroma Shift/Assets/Scripts/InputManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+
+	public static InputManager instance;
+	public static InputManager Instance
+	{
+		get
+		{
+			if (!instance) 
+				instance = GameObject.FindObjectOfType (typeof(InputManager)) as InputManager;
+
+			return instance;
+		}
+
+	}
+
+	//delegate for input
+	public delegate void KeyEvent();
+	public delegate void AxisEvent(float axisValue);
+
+	//Input events
+	public event KeyEvent Jump;
+	public event AxisEvent Run;
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		//if the Jump button is pushed and there are any subscribers
+		if (Input.GetButtonDown("Jump") && Jump != null)
+		{
+			//Call the Jump event
+			Jump();
+		}
+		//if there are any Run Subscribers
+		if (Run != null)
+		{
+			//Call the Run Event and link it to the horiztonal axis
+			Run(Input.GetAxis("Horizontal"));
+		}
+	}
+}
