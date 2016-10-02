b7697c0517159dd45d152e28aa5794f85e013bda
matbecker <7m4tt3r7@gmail.com>
2016-10-01 20:50:40 -0400

    Git fix.
    Replaced all files that were lost with my merge with my backed up
    version

---
+++ Chroma Shift/Assets/Scripts/Hero.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour {
+
+	[System.Serializable]
+	public class Stats
+	{
+		public int health;
+		public int attackPower;
+		public int defence;
+		public float currentShieldStrngth;
+		public float shieldCapacity;
+		public float attackRange;
+		public float attackSpeed;
+		public float maxVelocity;
+		public Vector2 jumpForce;
+		public Vector2 movementForce;
+
+	}
+
+	[SerializeField] Stats stats;
+	[SerializeField] Rigidbody2D rb;
+	[SerializeField] EdgeCollider2D col;
+	[SerializeField] LayerMask groundLayer;
+	// Use this for initialization
+	void Start () 
+	{
+		//register jump event
+		InputManager.Instance.Jump += Jump;
+		//register run event
+		InputManager.Instance.Run += Run;
+		//register attack event
+		InputManager.Instance.Attack += Attack;
+		//register Block event
+		InputManager.Instance.Block += Block;
+		//get the rigidbody of the gameobject
+		rb = gameObject.GetComponent<Rigidbody2D>();
+		//get the edge collider of the gameobject
+		col = gameObject.GetComponent<EdgeCollider2D>();
+		//ignore all layers but the ground layer
+		groundLayer = 1 << LayerMask.NameToLayer("Ground");
+
+	}
+	void OnDestroy () 
+	{
+		//unregister jump event
+		InputManager.Instance.Jump -= Jump;
+		//unregister run event
+		InputManager.Instance.Run -= Run;
+		//unregister attack event
+		InputManager.Instance.Attack -= Attack;
+		//unregister Block event
+		InputManager.Instance.Block -= Block;
+	}
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 down = transform.TransformDirection(Vector3.down) / 3;
+		Debug.DrawRay(new Vector2(transform.position.x, transform.position.y), down, Color.black);
+
+//		if (GroundCheck())
+//			Debug.Log("grounded!");
+	}
+	protected virtual void Jump()
+	{
+		if (GroundCheck())
+			rb.AddForce(stats.jumpForce);
+	}
+	protected virtual void Run(float horizontalAxis)
+	{
+		if (horizontalAxis > 0)
+		{
+			rb.AddForce(stats.movementForce);
+
+			//limit player velocity in right direction
+			if (rb.velocity.x > stats.maxVelocity)
+				rb.velocity = new Vector2(stats.maxVelocity, rb.velocity.y);
+		}
+		if (horizontalAxis < 0)
+		{
+			rb.AddForce(-stats.movementForce);
+
+			//limit players velocity in left direction
+			if (rb.velocity.x < -stats.maxVelocity) 
+				rb.velocity = new Vector2(-stats.maxVelocity, rb.velocity.y);
+		}
+	}
+	protected virtual void Attack()
+	{
+		Debug.Log("Attacking!");
+	}
+	protected virtual void Block()
+	{
+		Debug.Log("Blocking!");
+	}
+	protected virtual bool GroundCheck()
+	{
+		return Physics2D.OverlapCircle(col.bounds.center, 0.1f, groundLayer);
+	}
+
+}
---
+++ Chroma Shift/Assets/Scripts/InputManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+
+	public static InputManager instance;
+	public static InputManager Instance
+	{
+		get
+		{
+			if (!instance) 
+				instance = GameObject.FindObjectOfType (typeof(InputManager)) as InputManager;
+
+			return instance;
+		}
+
+	}
+
+	//delegate for input
+	public delegate void KeyEvent();
+	//public delegate void KeyEvent(float param);
+	public delegate void AxisEvent(float axisValue);
+
+	//Input events
+	public event KeyEvent Jump;
+	public event KeyEvent Attack;
+	public event KeyEvent Block;
+	public event KeyEvent Pause;
+	public event AxisEvent Run;
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+
+	void Update()
+	{
+		//if the Jump button is pushed and there are any subscribers
+		if (Input.GetButtonDown("Jump") && Jump != null)
+		{
+			//Call the Jump event
+			Jump();
+		}
+		//if the Attack Button is pushed and there are subscribers
+		if (Input.GetButtonDown("Attack") && Attack != null)
+		{
+			//Call the Attack event
+			Attack();
+		}
+		//if there are any Run Subscribers
+		if (Run != null)
+		{
+			//Call the Run Event and link it to the horiztonal axis
+			Run(Input.GetAxis("Horizontal"));
+		}
+		//if the Block Button is pushed and there are sunscribers
+		if (Input.GetButton("Block") && Block != null)
+		{
+			//Call the block event
+			Block();
+		}
+		//if the Pause Button is pushed and there are subscriberss
+		if (Input.GetButtonDown("Pause") && Pause != null)
+		{
+			//Call the pause event
+			Pause();
+		}
+	}
+}
