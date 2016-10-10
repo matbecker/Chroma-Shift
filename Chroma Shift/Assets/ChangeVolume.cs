744f0bcbec84c3ba217b56a06d0a18b89b322ff9
Daniel Argento <daniel.argento94@hotmail.com>
2016-10-10 15:32:28 -0400

    Completed Main Menu GUI
    Completed a functional Main Menu GUI. It still needs to be polished a
    bit more.

---
+++ Chroma Shift/Assets/ChangeVolume.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeVolume : MonoBehaviour {
+
+    AudioSource master;
+
+	// Use this for initialization
+	void Start () {
+        master = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setVolume(float value)
+    {
+        master.volume = value;
+    }
+}
---
+++ Chroma Shift/Assets/QuitOnClick.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitOnClick : MonoBehaviour {
+
+    /*This method utilizes compilation specific code. Which obviously, will determine which one to use during compile
+     time.*/
+    public void Quit()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
